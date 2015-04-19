2b511803878471271c7ad8e7dab1f4292f940f93
Unlink <miso250593@gmail.com>
2015-04-19 11:11:15 +0200

    Agent okolia generatory

--- src/agents/AgentOkolia.java
+++ src/agents/AgentOkolia.java
@@ -1,18 +1,32 @@
 package agents;
 
 import OSPABA.*;
+import OSPRNG.ExponentialRNG;
+import OSPRNG.RNG;
 import simulation.*;
 import managers.*;
 import continualAssistants.*;
+import entity.Zastavka;
 import instantAssistants.*;
 
 //meta! id="2"
 public class AgentOkolia extends Agent
 {
+	
+	private final RNG[] aGeneratoryPrichodov;
+	
 	public AgentOkolia(int id, Simulation mySim, Agent parent)
 	{
 		super(id, mySim, parent);
 		init();
+		aGeneratoryPrichodov = new RNG[((MySimulation)mySim).getZastavky().size()];
+		for(Zastavka z:((MySimulation)mySim).getZastavky()) {
+			aGeneratoryPrichodov[z.getId()] = new ExponentialRNG(z.getPocLudi()/65d);
+		}
+	}
+	
+	public RNG getRngFor(int z) {
+		return aGeneratoryPrichodov[z];
 	}
 
 	//meta! userInfo="Generated code: do not modify", tag="begin"
